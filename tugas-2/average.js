0ba99da5792c47befa4e50effd6b7a432545a081
reezyx <rudiansyahpratama04@gmail.com>
2024-03-13 13:41:59 +0700

    tugas 2 javascript

---
+++ tugas-2/average.js
@@ -0,0 +1,9 @@
+/*
+    Terdapat sebuah array dibawah bernama arr.
+    1. Cobalah untuk mencari rata-rata dari array tersebut dengan menggunakan for loop (bukan dihitung manual)
+    2. Lalu tampilkan hasil rata-rata tersebut pada terminal
+*/
+
+var arr = [
+  52, 10, 34, 98, 75, 84, 102, 73, 86, 95, 42, 52, 68, 94, 43, 87, 123,
+];
