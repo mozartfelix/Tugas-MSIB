2fb9e5f3966e470c99b075f4389ba5503ac93222
Farhan Abdul Hamid <arhandev.code@gmail.com>
2024-03-14 13:38:34 +0700

    add tugas 3

---
+++ tugas-3/fetchData.js
@@ -0,0 +1,8 @@
+/*
+    1. Jalankan sebuah function fetch (dapat menggunakan then atau async await) untuk mengambil data pada URL berikut:
+    https://api.amandemy.co.id/api/tugas
+    2. Kamu akan mendapatkan data list makanan beserta harganya.
+    3. Setelah berhasil mendapatkan data dari server, coba lakukan filter dengan ketentuan harga makanan yang >= 15000
+    4. Tampilkan hasil filter tersebut pada terminal
+    tips: caranya mirip seperti yang ada di materi, namun dengan tambahan filter
+*/
